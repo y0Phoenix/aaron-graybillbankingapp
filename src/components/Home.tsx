8926e9b646a6a9d9eb5da43979e4c1af60c9988a
Aaron <aarongraybill3@gmail.com>
2022-04-25 11:39:39 -0500

    finish home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import bank from '../images/BIZ-bank28-inside.webp';
+import { Link } from 'react-router-dom';
+
+const initial = {
+	opacity: 0
+};
+const animate = {
+	opacity: 1
+};
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+	return (
+		<div className='home-container'>
+			<div className="card" style={{width: '45rem', textAlign: 'center'}}>
+				<motion.img initial={initial} animate={animate} transition={{duration: 1}} src={bank} className='card-img-top'></motion.img>
+				<div className="card-body">
+					<motion.h1 initial={initial} animate={animate} transition={{duration: 2}} className="card-title">Welcome To Bad Bank</motion.h1>
+					<motion.p initial={initial} animate={animate} transition={{duration: 2.5}} className="card-text">For All Your Banking Needs</motion.p>
+					<motion.div initial={initial} animate={animate} transition={{duration: 3}}>
+						<motion.div whileHover={{scale: 1.07}}>
+							<Link to="/createaccount" className="btn btn-primary">
+								Open An Account Here
+							</Link>
+						</motion.div>
+					</motion.div>
+				</div>
+		</div>
+		</div>
+	)
 }
 
 export default Home
