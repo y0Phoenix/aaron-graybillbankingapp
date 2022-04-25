22ee0271848a16272dfb3865333445957a1f6c45
Aaron <aarongraybill3@gmail.com>
2022-04-24 20:45:05 -0500

    init create account page

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const CreateAccount = () => {
-  return (
-    <div>CreateAccount</div>
-  )
+    return (
+        <div className='createaccount-main'>
+            <div>
+                <h1>Create Account</h1>
+            </div>
+            <div className='card' style={{width: "18rem"}}>
+                <div className='card-body center'>
+                    <input type={'text'} className="card-title" placeholder='Name'></input>
+                    <input type={'text'} className="card-title" placeholder='Email'></input>
+                    <input type={'text'} className="card-title" placeholder='Password'></input>
+                    <input type={'button'} className="btn btn-primary" value={'Create\nAccount'}></input>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default CreateAccount
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,10 +28,6 @@ const Navbar = () => {
           <Link className="nav-link" to='/alldata'>All Data</Link>
         </li>
       </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
