34bec63621cf024307160cfccce08e45713abb5e
Aaron <aarongraybill3@gmail.com>
2022-04-24 20:00:37 -0500

    init build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { Provider } from 'react-redux'
