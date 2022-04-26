f16a8103d548d82037ae40bbd2c9c98bbb2e7bbd
Aaron <aarongraybill3@gmail.com>
2022-04-25 21:01:51 -0500

    highlights nav of whichever page its on

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
 interface Desc {
@@ -8,6 +8,7 @@ interface Desc {
 }
 
 const Navbar = () => {
+	const location = useLocation();
 	const [show, setShow] = useState({
 		home: false,
 		createAccount: false,
@@ -16,6 +17,7 @@ const Navbar = () => {
 		allData: false
 	});
 	const [checkShow, setCheckShow] = useState('');
+	const [active, setActive] = useState('home');
 
 	useEffect(() => {
 		if (checkShow == '') return;
@@ -23,6 +25,14 @@ const Navbar = () => {
 		if (bool) setShow({...show, [checkShow]: false});
 		setCheckShow('');
 	}, [checkShow]);
+
+	useEffect(() => {
+		if (location.pathname === '/') setActive('home');
+		else if (location.pathname.includes('createaccount')) setActive('createaccount');
+		else if (location.pathname.includes('withdraw')) setActive('withdraw');
+		else if (location.pathname.includes('deposit')) setActive('deposit');
+		else if (location.pathname.includes('alldata')) setActive('alldata');
+	}, [location]);
 
 	const toggleDesc = (type: string) => {
 		setTimeout(() => setCheckShow(type), 3000);
@@ -40,9 +50,9 @@ const Navbar = () => {
 
 	return (
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-			<div className="container-fluid">
-				<div className='nav-item-container'>
-					<Link onMouseOver={() => toggleDesc('home')} onMouseLeave={() => toggleDesc('home')} className="navbar-brand" to='/'>Bad Bank</Link>
+			<div className="container-fluid" style={{justifyContent: 'center'}}>
+				<div className={`nav-item-container ${active === 'home' && 'active'} main`}>
+					<Link onMouseOver={() => toggleDesc('home')} onMouseLeave={() => toggleDesc('home')} className="navbar-brand acive" to='/'>Bad Bank</Link>
 					<Description type={'home'} text={'Home Page'}/>
 				</div>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -51,25 +61,25 @@ const Navbar = () => {
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 					<li className="nav-item">
-						<div className='nav-item-container'>
+						<div className={`nav-item-container ${active === 'createaccount' && 'active'}`}>
 							<Link className="nav-link" to='/createaccount' onMouseOver={() => toggleDesc('createAccount')} onMouseLeave={() => toggleDesc('createAccount')}>Create Account</Link>
 							<Description type='createAccount' text='Create A Bank Account To Get Started'/>
 						</div>
 					</li>
-					<li className="nav-item dropdown">
+					<li className={`nav-item dropdown ${active === 'withdraw' ? 'active' : active === 'deposit' && 'active'}`}>
 						<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 						Actions
 						</a>
 						<ul className="dropdown-menu" aria-labelledby="navbarDropdown">
 						<li>
-							<div className='nav-item-container'>
+							<div className={`nav-item-container ${active === 'deposit' && 'active-light'}`}>
 								<Link className="dropdown-item" to='/deposit' onMouseOver={() => toggleDesc('deposit')} onMouseLeave={() => toggleDesc('deposit')}>Deposit</Link>
 								<Description type='deposit' text='Deposit Money Into Your Account'/>
 							</div>
 						</li>
 						<li><hr className="dropdown-divider"></hr></li>
 						<li>
-							<div className='nav-item-container'>
+							<div className={`nav-item-container ${active === 'withdraw' && 'active-light'}`}>
 								<Link className="dropdown-item" to='/withdraw' onMouseOver={() => toggleDesc('withdraw')} onMouseLeave={() => toggleDesc('withdraw')}>Withdraw</Link>
 								<Description type='withdraw' text='Withdraw Money From Your Account'/>
 							</div>
@@ -77,7 +87,7 @@ const Navbar = () => {
 						</ul>
 					</li>
 					<li className="nav-item">
-						<div className='nav-item-container'>
+						<div className={`nav-item-container ${active === 'alldata' && 'active'}`}>
 							<Link className="nav-link" to='/alldata' onMouseOver={() => toggleDesc('allData')} onMouseLeave={() => toggleDesc('allData')}>All Data</Link>
 							<Description type='allData' text='View All Account Data' />
 						</div>
